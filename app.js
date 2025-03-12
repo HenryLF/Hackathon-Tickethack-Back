8af289d55d9cbfc625bd55af40e845c2046b1dc0
HenryLF <henrylf@protonmail.com>
2025-03-12 08:34:19 +0000

    add db connection

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require("dotenv").config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require("./models/connection")
+
+
+var tripRouter = require("./routes/trips")
+
 
 var app = express();
 
@@ -17,7 +21,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/trips', tripRouter);
 
 module.exports = app;
